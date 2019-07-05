d437359339aef94c281679cefc4d1d0b83524c7c
irshadvali <irshad.vali@gmail.com>
2019-07-05 17:57:32 +0530

    image 2

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/irshad",require("./routes/routes"));
 Not found Route
 */
 // app.use(express.static('public'))
-app.use("/public", express.static(path.join(__dirname, "..", "public")));
+// app.use("/public", express.static(path.join(__dirname, "..", "public")));
 app.use((req,res,next)=>{
     req.status=404;
     const error=new Error("Routes not found");
